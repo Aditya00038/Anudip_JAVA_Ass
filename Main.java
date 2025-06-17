706464de99cb0e0b5a511251f48e5f8519bbdfd6
Aditya00038 <adityasuryawanshi038@gmail.com>
2025-06-17 23:04:35 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        int age = 18;                  // Integer variable
+        double salary = 43430.35;      // Decimal number
+        char grade = 'A';              // Character
+        boolean passed = true;         // Boolean value
+        String name = "Aditya";         // String (non-primitive)
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Salary: " + salary);
+        System.out.println("Grade: " + grade);
+        System.out.println("Passed: " + passed);
+    }
+}
---
+++ Ternary.java
@@ -0,0 +1,10 @@
+public class Ternary {
+    public static void main(String[] args) {
+        int daysInFebruary = 29;
+        String result;
+        // ternary operator
+        result = (daysInFebruary == 28) ? "Not a leap year" : "Leap year";
+
+        System.out.println(result);
+    }
+}
